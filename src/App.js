87785a5f834b4123b67a5f0d7fdf7ce7f5041372
rheajacob02 <21rhea11jacob02@gmail.com>
2023-01-27 01:28:08 +0530

    created function to render a component on click

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
+import React, {Component} from 'react';
 import './App.css';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent';
+import {Dishes} from './shared/dishes';
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className='container'>
-          <NavbarBrand href="/">Risrorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu/>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state={
+      dishes: Dishes
+    };
+  }
+
+  render(){
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className='container'>
+            <NavbarBrand href="/">Risrorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes}/>
+      </div>
+    );
+  }
 }
 
 export default App;
